13ee6024cdaca06e238a580e76baa3da8bd2ed44
Shivam1938 <boymax1938@gmail.com>
2026-07-02 01:18:48 +0530

    commit

--- src/main/java/com/springboot/CRUD_springboot/entity/Student.java
+++ src/main/java/com/springboot/CRUD_springboot/entity/Student.java
@@ -1,18 +1,19 @@
 package com.springboot.CRUD_springboot.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
 @Data
 public class Student {
 
-    @Id()
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
